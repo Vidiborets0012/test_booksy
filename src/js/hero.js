087baab179eccf2ccc82fea2ceec5ac03260a8e6
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-10-10 18:53:10 +0300

    add styles for disabled button, remove focus after click, and add keyboard control

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,14 +1,60 @@
 import Swiper from 'swiper/bundle';
 
 const swiper = new Swiper('.hero-slider', {
-  // Optional parameters
-  // direction: 'vertical',
-  // loop: true,
+  slidesPerView: 1,
   slidesPerGroup: 1,
 
-  // Navigation arrows
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
   navigation: {
     nextEl: '.hero-button-next',
     prevEl: '.hero-button-prev',
   },
 });
+
+const prevButton = document.querySelector('.hero-button-prev');
+const nextButton = document.querySelector('.hero-button-next');
+
+function handleButtonClick(button) {
+  if (button) {
+    window.requestAnimationFrame(() => button.blur());
+  }
+}
+
+if (prevButton) {
+  prevButton.addEventListener('click', function () {
+    handleButtonClick(this);
+  });
+}
+
+if (nextButton) {
+  nextButton.addEventListener('click', function () {
+    handleButtonClick(this);
+  });
+}
+
+/*
+document.addEventListener('keydown', function (event) {
+  const focusedElement = document.activeElement;
+
+  const isHeroSliderFocused =
+    focusedElement && focusedElement.closest('.hero-slider');
+
+  if (!isHeroSliderFocused) {
+    return;
+  }
+
+  switch (event.key) {
+    case 'ArrowLeft':
+      swiperHero.slidePrev();
+      break;
+    case 'ArrowRight':
+      swiperHero.slideNext();
+      break;
+  }
+});
+
+*/
